a14b47bab5a25b0f24289837b3ddb96dbc4f2ead
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2023-12-19 21:15:58 +0530

    Sidebar designed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { Route, Routes } from "react-router-dom";
+import PageLayout from "./layouts/Page/PageLayout";
 import AuthPage from "./pages/Auth/AuthPage";
 import HomePage from "./pages/Home/HomePage";
 
 function App() {
   return (
-    <>
+    <PageLayout>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/auth" element={<AuthPage />} />
       </Routes>
-    </>
+    </PageLayout>
   );
 }
 
